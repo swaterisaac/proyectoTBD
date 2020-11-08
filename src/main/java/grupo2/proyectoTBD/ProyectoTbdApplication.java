4a0fc4f4dec773e53e59d260e5dfe1d1514cf85b
Isaac Rojas <isaac.rojas@usach.cl>
2020-11-08 17:43:25 -0300

    Proyecto base de spring init

---
+++ src/main/java/grupo2/proyectoTBD/ProyectoTbdApplication.java
@@ -0,0 +1,13 @@
+package grupo2.proyectoTBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grupo2/proyectoTBD/ProyectoTbdApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo2.proyectoTBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
