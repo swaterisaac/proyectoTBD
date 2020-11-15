44c36079365a7892fe49e22b1c4a5843b5efdd6d
Isaac Rojas <isaac.rojas@usach.cl>
2020-11-15 19:01:38 -0300

    Modelo instution

---
+++ src/main/java/grupo2/proyectoTBD/models/Institution.java
@@ -0,0 +1,22 @@
+package grupo2.proyectoTBD.models;
+
+public class Institution{
+    private Long id;
+    private String name;
+
+    public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
