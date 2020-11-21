0801186b36fd54875d0b25ce04467e793b5edd90
Belén Rickmers <belen.rickmers@usach.cl>
2020-11-21 14:28:25 -0300

    Front funcional
    Con Florencia y Jorge

--- frontdos/src/views/Tarea.vue
+++ frontdos/src/views/Tarea.vue
@@ -5,7 +5,7 @@
       class="mx-auto"
       width="300">
 
-      <v-card-title class= "justify-center mb-2">Nombre tarea {{$route.params.id}}</v-card-title>
+      <v-card-title class= "justify-center mb-2"> {{tarea.name}}</v-card-title>
 
       <!---<v-img
         height="200px"
@@ -16,35 +16,21 @@
       
 
       <v-card-subtitle class="pb-2">
-        id emergencia
+        Id emergencia asociada: {{tarea.id_emergency}}
       </v-card-subtitle>
 
       <v-card-text class="text--primary">
-        <div>{{tarea.name}}</div>
+        <div>Descripción tarea : {{tarea.description}}</div>
       </v-card-text>
-      <v-container id="dropdown-example-3" fluid style = "width: 80%">
-        <v-overflow-btn
-          class="my-2"
-          :items="dropdown_font"
-          :label="statusActual"
-          target="#dropdown-example-1"
-        ></v-overflow-btn>
-      </v-container>
+
 
       <v-container>
-        <span>Seleccionado: {{ selected }}</span>
-        <select v-model="selected">
-          <option v-for="stat in statuses" :key="stat.id" v-bind:value="stat.id">
-            {{ stat.description }}
-          </option>
-        </select>
-        
+        <v-select :items="statuses" v-model="selected" :label="statusActual" item-text="description" item-value="id" solo>
+        </v-select>
       </v-container>
+
       <v-card-actions>
-        <v-btn
-          color="orange"
-          text
-          >
+        <v-btn color="orange" text v-on:click="editStatus">
           Guardar estado
         </v-btn>
       </v-card-actions>
@@ -57,32 +43,36 @@
 
   export default {
     data: () => ({
-      //idTarea = null,
-      dropdown_font: [],
       tarea: [],
       status:[],
       statuses:[],
       selected:'',
       statusActual: '',
-      variable: ''
+      variable: '',
+      idEmergencia:[]
     }),
+
+    methods:{
+      editStatus: function(){axios.post('http://localhost:1818/task/edit/'+ this.tarea.id, {id_status: 1})},
+    },
 
     created(){ 
       axios.get('http://localhost:1818/task/' + this.$route.params.id).then(response=>{
       this.tarea = response.data;
+      this.idEmergencia = response.data.id_emergencia;
       this.variable = JSON.stringify(this.tarea.id_status);
       console.log(this.variable);
       console.log(this.tarea.id_status);
       axios.get('http://localhost:1818/status/' + this.variable).then(response=>{
       this.status = response.data;
-      this.statusActual = this.status.description;
-      
-    })
-    }),
-    axios.get('http://localhost:1818/status/').then(response=>{
-      this.statuses = response.data;
-      console.log(this.statuses);
-    })
+      this.statusActual = this.status.description; 
+      })
+      }),
+
+      axios.get('http://localhost:1818/status/').then(response=>{
+        this.statuses = response.data;
+        console.log(this.statuses);
+      })
     
     }
 
