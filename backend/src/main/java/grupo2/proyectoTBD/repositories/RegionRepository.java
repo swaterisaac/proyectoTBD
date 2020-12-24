97019e789d0ebb690413e421ff6e0a7a44d693f5
Isaac Rojas <isaac.rojas@usach.cl>
2020-12-24 03:55:51 -0300

    beta de usuarios por region

--- backend/src/main/java/grupo2/proyectoTBD/repositories/RegionRepository.java
+++ backend/src/main/java/grupo2/proyectoTBD/repositories/RegionRepository.java
@@ -1,8 +1,10 @@
 package grupo2.proyectoTBD.repositories;
 
 import com.google.gson.JsonObject;
+import grupo2.proyectoTBD.models.Emergency;
 import grupo2.proyectoTBD.models.Region;
 import grupo2.proyectoTBD.models.Skill;
+import grupo2.proyectoTBD.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.sql2o.Connection;
@@ -20,6 +22,27 @@ public class RegionRepository {
                 "SELECT gid,nom_reg FROM division_regional;";
         try(Connection con = sql2o.open()) {
             return con.createQuery(sql).executeAndFetch(Region.class);
+        }
+    }
+
+    public Region getRegion(Long id){
+        String sql =
+                "SELECT gid,nom_reg "  +
+                "FROM division_regional where gid = :id";
+        try(Connection con = sql2o.open()) {
+            return con.createQuery(sql).addParameter("id",id).executeAndFetchFirst(Region.class);
+        }
+    }
+    public List<User> getUsers(Long regionId){
+        String sql = "SELECT id, nombre, st_y(st_astext(location)) AS latitude,st_x(st_astext(location)) AS longitude" +
+                " FROM users AS u " +
+                "INNER JOIN division_regional AS r " +
+                "ON ST_WITHIN(u.location, r.geom) " +
+                "WHERE r.gid = :regionId";
+        try(Connection con = sql2o.open()) {
+            return con.createQuery(sql)
+            .addParameter("regionId",regionId)
+            .executeAndFetch(User.class);
         }
     }
 
--- backend/src/main/java/grupo2/proyectoTBD/services/RegionService.java
+++ backend/src/main/java/grupo2/proyectoTBD/services/RegionService.java
@@ -2,12 +2,16 @@ package grupo2.proyectoTBD.services;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
+import grupo2.proyectoTBD.models.Emergency;
 import grupo2.proyectoTBD.models.Region;
+import grupo2.proyectoTBD.models.User;
 import grupo2.proyectoTBD.repositories.RegionRepository;
+import grupo2.proyectoTBD.repositories.UserRepository;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +22,12 @@ import java.util.List;
 public class RegionService {
 
     private final grupo2.proyectoTBD.repositories.RegionRepository regionRepository;
+    private final UserRepository userRepository;
     private final Gson gson;
 
-    RegionService(RegionRepository regionRepository){
+    RegionService(RegionRepository regionRepository, UserRepository userRepository){
         this.regionRepository = regionRepository;
+        this.userRepository = userRepository;
         this.gson = new GsonBuilder().setPrettyPrinting().create();
     }
 
@@ -29,6 +35,25 @@ public class RegionService {
     ResponseEntity<List<Region>>getAllRegions(){
         return new ResponseEntity<List<Region>>(
                 this.regionRepository.getRegions(),
+                HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<String> getEmergency(@PathVariable Long id){
+        Region region = this.regionRepository.getRegion(id);
+        if(region != null){
+            return new ResponseEntity<>(
+                    gson.toJson(region),
+                    HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/user/{regionId}")
+    ResponseEntity<String> getUsersFromRegion(@PathVariable Long regionId){
+        List<User> users = this.regionRepository.getUsers(regionId);
+        return new ResponseEntity<String>(
+                gson.toJson(users),
                 HttpStatus.OK);
     }
 }
