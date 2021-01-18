8954be3b1b5da27f48a86c9025b8e989ab427684
Esteban López <esteban.lopez.g@usach.cl>
2021-01-18 15:14:27 -0300

    Modifica el repositorio para eliminar la configuracion de MongoDB

--- backend/src/main/java/grupo2/proyectoTBD/repositories/MongoDBConfig.java
+++ backend/src/main/java/grupo2/proyectoTBD/repositories/MongoDBConfig.java
@@ -3,6 +3,7 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
@@ -12,6 +13,8 @@ import java.util.Collections;
 
     @Configuration
     public class MongoDBConfig extends AbstractMongoClientConfiguration {
+        @Value("${spring.data.mongodb.uri}")
+        private String mongoDBConn;
         @Override
         protected String getDatabaseName() {
             return "tbd";
@@ -19,7 +22,7 @@ import java.util.Collections;
 
         @Override
         public MongoClient mongoClient() {
-            ConnectionString connectionString = new ConnectionString(***REMOVED***);
+            ConnectionString connectionString = new ConnectionString(mongoDBConn);
             MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                     .applyConnectionString(connectionString)
                     .build();
